7a4091f67179005850bb44380f5225514c62772a
Soe H Aung <93974425+SoeHtutAung@users.noreply.github.com>
2025-03-19 21:56:49 +0000

    Update and rename data_preparation.R to 1_dataprep.R

---
+++ scripts/1_dataprep.R
@@ -0,0 +1,7 @@
+###########################
+# Title: Data preparation
+# Purpose: Install and load libraries. Extract and load datasets.
+###########################
+
+# install and load libraries
+
