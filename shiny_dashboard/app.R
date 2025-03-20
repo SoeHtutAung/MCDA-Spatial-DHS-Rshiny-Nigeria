6eaaa49a5801d8c0b4edfeba316eb679a3a09494
Soe H Aung <93974425+SoeHtutAung@users.noreply.github.com>
2025-03-20 21:45:46 +0000

    Create app.R

---
+++ shiny_dashboard/app.R
@@ -0,0 +1,7 @@
+## load necessary r files
+source ("data/process.R") # to load necessary packages and datasets
+source ("ui.R")
+source ("server.R")
+
+## run app
+shinyApp(ui = ui, server = server)
